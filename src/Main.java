dee4cd5bc299a0e2eb46303fb22e21c8df06318e
vlifanova <vlifanova2@alfabank.ru>
2022-05-24 16:22:41 +0300

    Операции с директориями и файлами

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 /**
  * Created by Vera Lifanova
@@ -10,95 +9,113 @@ import java.net.URISyntaxException;
  */
 
 public class Main {
-    public static final String ROOT_PATH = "C://Games";
-
+    public static final String ROOT_PATH = "C:";
+    public static StringBuilder builder = new StringBuilder();
 
     public static void main(String[] args) {
-        StringBuilder builder = new StringBuilder();
-
-        File rootDir = new File(ROOT_PATH);
-
-        if (rootDir.mkdir()) {
-             builder.append("[MAIN]: создан каталог Games").append("\n");
-        }
+        // Создаем Games
+        File rootDir = createDir(ROOT_PATH, "Games");
 
         String path = rootDir.getAbsolutePath();
-
-        // src, res, savegames, temp
-        File srcDir = new File(path + "\\" + "src");
-        File resDir = new File(path + "\\" + "res");
-        File saveGamesDir = new File(path + "\\" + "savegames");
-        File tempDir = new File(path + "\\" + "temp");
-
-        if (srcDir.mkdir()) {
-            builder.append("[MAIN]: создан каталог src").append("\n");
-        }
-        // main, test
-        File mainDir = new File(srcDir.getAbsolutePath() + "\\" + "main");
-        if (mainDir.mkdir()) {
-            builder.append("[MAIN]: создан каталог main").append("\n");
-        }
-
-        try {
-            File mainFile =  new File(mainDir.getAbsolutePath()+ "\\" + "Main.java");
-            if(mainFile.createNewFile()) {
-                builder.append("[MAIN]: создан файл Main.java").append("\n");
-            }
-
-            File utilsFile =  new File(mainDir.getAbsolutePath()+ "\\" + "Utils.java");
-            if(utilsFile.createNewFile()) {
-                builder.append("[MAIN]: создан файл Utils.java").append("\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        File testDir = new File(srcDir.getAbsolutePath() + "\\" + "test");
-        if (testDir.mkdir()) {
-            builder.append("[MAIN]: создан каталог test").append("\n");
-        }
-
-        //
-        if (resDir.mkdir()) {
-            builder.append("[MAIN]: создан каталог res").append("\n");
-        }
-
-        File drawablesDir = new File(resDir.getAbsolutePath() + "\\" + "drawables");
-        if (drawablesDir.mkdir()) {
-            builder.append("[MAIN]: создан каталог drawables").append("\n");
-        }
-
-        File vectorsDir = new File(resDir.getAbsolutePath() + "\\" + "vectors");
-        if (vectorsDir.mkdir()) {
-            builder.append("[MAIN]: создан каталог vectors").append("\n");
-        }
-
-        File iconsDir = new File(resDir.getAbsolutePath() + "\\" + "icons");
-        if (iconsDir.mkdir()) {
-            builder.append("[MAIN]: создан каталог icons").append("\n");
-        }
-
-        //
-        if (saveGamesDir.mkdir()) {
-            builder.append("[MAIN]: создан каталог savegames").append("\n");
-        }
-
-        if (tempDir.mkdir()) {
-            builder.append("[MAIN]: создан каталог temp").append("\n");
-        }
-
-        try {
-            File tempFile =  new File(tempDir.getAbsolutePath()+ "\\" + "temp.txt");
-            if(tempFile.createNewFile()) {
-                builder.append("[MAIN]: создан файл temp.txt").append("\n");
-            }
-
-            FileWriter fileWriter = new FileWriter(tempFile);
-            fileWriter.write(builder.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Создаем src и все вложенные папки и файлы
+        createSrc(path);
+        // Создаем res и все вложенные папки
+        createRes(path);
+        // Создаем savegames
+        createSaveGames(path);
+        // Создаем temp
+        createTemp(path);
 
         System.out.println(builder.toString());
+    }
+
+    private static void createSrc(String path) {
+        // Создаем src
+        File srcDir = createDir(path, "src");
+
+        // Создаем main и файлы Main.java, Utils.java
+        File mainDir = createDir(srcDir.getAbsolutePath(), "main");
+        createFile(mainDir.getAbsolutePath(), "Main.java");
+        createFile(mainDir.getAbsolutePath(), "Utils.java");
+
+        // Создаем test
+        createDir(srcDir.getAbsolutePath(), "test");
+    }
+
+    private static void createRes(String path) {
+        // Создаем res
+        File resDir = createDir(path, "res");
+        // Создаем drawables, vectors,icons
+        createDir(resDir.getAbsolutePath(), "drawables");
+        createDir(resDir.getAbsolutePath(), "vectors");
+        createDir(resDir.getAbsolutePath(), "icons");
+    }
+
+    private static void createSaveGames(String path) {
+        createDir(path, "savegames");
+    }
+
+    private static void createTemp(String path) {
+        File tempDir = createDir(path, "temp");
+
+        // Создаем temp.txt
+        File tempFile = createFile(tempDir.getAbsolutePath(), "temp.txt");
+        writeToFile(tempFile, builder.toString());
+    }
+
+    /**
+     * Создание директории
+     *
+     * @param path - абсолютный путь к файлу
+     * @param name - название создаваемого файла
+     * @return File
+     */
+    private static File createDir(String path, String name) {
+        String pathname = path + "\\" + name;
+
+        File resultDir = new File(pathname);
+
+        if (resultDir.mkdir()) {
+            builder.append("[MAIN]: создан каталог ").append(name).append("\n");
+        }
+
+        return resultDir;
+    }
+
+    /**
+     * Создание файла
+     *
+     * @param path     - абсолютный путь к файлу
+     * @param filename - название создаваемого файла
+     * @return File
+     */
+    private static File createFile(String path, String filename) {
+        String pathname = path + "\\" + filename;
+
+        File resultFile = null;
+        try {
+            resultFile = new File(pathname);
+            if (resultFile.createNewFile()) {
+                builder.append("[MAIN]: создан файл ").append(filename).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return resultFile;
+    }
+
+    /**
+     * Запись произвольного текста в файл
+     *
+     * @param file - объект File, представляющий файл для записи
+     * @param text - текст, записываемый в файл
+     */
+    private static void writeToFile(File file, String text) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+            bw.write(text);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
